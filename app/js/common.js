714572b1e73e5242912cb0d305df41075a62b28c
wsl <382159263@qq.com>
2016-11-01 17:49:07 +0800

    头部底部公共样式

---
+++ app/js/common.js
@@ -0,0 +1,42 @@
+
+//底部高亮
+$(function(){
+	$("#homeicon1").css({"color":"#8e488e"});
+	$(".footer li").eq(0).click(function(){
+		$("#homeicon1").css({"color":"#8e488e"});
+		$("#homeicon2").css({"color":"#777"});
+		$("#homeicon3").css({"color":"#777"});
+		$("#homeicon4").css({"color":"#777"});
+		$("#homeicon5").css({"color":"#777"});
+	})
+	$(".footer li").eq(1).click(function(){
+		$("#homeicon1").css({"color":"#777"});
+		$("#homeicon2").css({"color":"#8e488e"});
+		$("#homeicon3").css({"color":"#777"});
+		$("#homeicon4").css({"color":"#777"});
+		$("#homeicon5").css({"color":"#777"});
+	})
+	$(".footer li").eq(2).click(function(){
+		$("#homeicon1").css({"color":"#777"});
+		$("#homeicon2").css({"color":"#777"});
+		$("#homeicon3").css({"color":"#8e488e"});
+		$("#homeicon4").css({"color":"#777"});
+		$("#homeicon5").css({"color":"#777"});
+	})
+	$(".footer li").eq(3).click(function(){
+		$("#homeicon1").css({"color":"#777"});
+		$("#homeicon2").css({"color":"#777"});
+		$("#homeicon3").css({"color":"#777"});
+		$("#homeicon4").css({"color":"#8e488e"});
+		$("#homeicon5").css({"color":"#777"});
+	})
+	$(".footer li").eq(4).click(function(){
+		$("#homeicon1").css({"color":"#777"});
+		$("#homeicon2").css({"color":"#777"});
+		$("#homeicon3").css({"color":"#777"});
+		$("#homeicon4").css({"color":"#777"});
+		$("#homeicon5").css({"color":"#8e488e"});
+	})
+	
+})
+	
--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,6 @@
 window.onload = function(){
+	
+	//轮播图
 	var mySwiper = new Swiper('.swiper-container', {
 		autoplay: 2000,//可选选项，自动滑动
 		pagination : '.swiper-pagination',
@@ -96,45 +98,50 @@ window.onload = function(){
 		})
 	})
 	
-	//底部高亮
+//	//底部高亮
+//	$(function(){
+//		$("#homeicon1").css({"color":"#8e488e"});
+//		$(".footer li").eq(0).click(function(){
+//			$("#homeicon1").css({"color":"#8e488e"});
+//			$("#homeicon2").css({"color":"#777"});
+//			$("#homeicon3").css({"color":"#777"});
+//			$("#homeicon4").css({"color":"#777"});
+//			$("#homeicon5").css({"color":"#777"});
+//		})
+//		$(".footer li").eq(1).click(function(){
+//			$("#homeicon1").css({"color":"#777"});
+//			$("#homeicon2").css({"color":"#8e488e"});
+//			$("#homeicon3").css({"color":"#777"});
+//			$("#homeicon4").css({"color":"#777"});
+//			$("#homeicon5").css({"color":"#777"});
+//		})
+//		$(".footer li").eq(2).click(function(){
+//			$("#homeicon1").css({"color":"#777"});
+//			$("#homeicon2").css({"color":"#777"});
+//			$("#homeicon3").css({"color":"#8e488e"});
+//			$("#homeicon4").css({"color":"#777"});
+//			$("#homeicon5").css({"color":"#777"});
+//		})
+//		$(".footer li").eq(3).click(function(){
+//			$("#homeicon1").css({"color":"#777"});
+//			$("#homeicon2").css({"color":"#777"});
+//			$("#homeicon3").css({"color":"#777"});
+//			$("#homeicon4").css({"color":"#8e488e"});
+//			$("#homeicon5").css({"color":"#777"});
+//		})
+//		$(".footer li").eq(4).click(function(){
+//			$("#homeicon1").css({"color":"#777"});
+//			$("#homeicon2").css({"color":"#777"});
+//			$("#homeicon3").css({"color":"#777"});
+//			$("#homeicon4").css({"color":"#777"});
+//			$("#homeicon5").css({"color":"#8e488e"});
+//		})
+//		
+//	})
+//	
+	//lazyload延迟加载
 	$(function(){
-		$("#homeicon1").css({"color":"#8e488e"});
-		$(".footer li").eq(0).click(function(){
-			$("#homeicon1").css({"color":"#8e488e"});
-			$("#homeicon2").css({"color":"#777"});
-			$("#homeicon3").css({"color":"#777"});
-			$("#homeicon4").css({"color":"#777"});
-			$("#homeicon5").css({"color":"#777"});
-		})
-		$(".footer li").eq(1).click(function(){
-			$("#homeicon1").css({"color":"#777"});
-			$("#homeicon2").css({"color":"#8e488e"});
-			$("#homeicon3").css({"color":"#777"});
-			$("#homeicon4").css({"color":"#777"});
-			$("#homeicon5").css({"color":"#777"});
-		})
-		$(".footer li").eq(2).click(function(){
-			$("#homeicon1").css({"color":"#777"});
-			$("#homeicon2").css({"color":"#777"});
-			$("#homeicon3").css({"color":"#8e488e"});
-			$("#homeicon4").css({"color":"#777"});
-			$("#homeicon5").css({"color":"#777"});
-		})
-		$(".footer li").eq(3).click(function(){
-			$("#homeicon1").css({"color":"#777"});
-			$("#homeicon2").css({"color":"#777"});
-			$("#homeicon3").css({"color":"#777"});
-			$("#homeicon4").css({"color":"#8e488e"});
-			$("#homeicon5").css({"color":"#777"});
-		})
-		$(".footer li").eq(4).click(function(){
-			$("#homeicon1").css({"color":"#777"});
-			$("#homeicon2").css({"color":"#777"});
-			$("#homeicon3").css({"color":"#777"});
-			$("#homeicon4").css({"color":"#777"});
-			$("#homeicon5").css({"color":"#8e488e"});
-		})
-		
+		$("img").lazyload({effect: "fadeIn"});
 	})
 	
 	
