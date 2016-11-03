52767ac6e2dfa111dee0eda6da5dbb50decc3bb4
XTL <543094330@qq.com>
2016-11-03 14:24:24 +0800

    整改

--- app/js/myorder.js
+++ app/js/myorder.js
@@ -1,7 +1,5 @@
 (function(){
 	document.addEventListener('DOMContentLoaded',function(){
-		
-	
 	var $order_details = $('.order_details');
 	//本地存储
 //	var goods = []
@@ -22,11 +20,11 @@
 			var $div = $('<div/>');
 			var $header = $('<div/>').addClass('header').html('<span>'+'店铺一'+'</span>'+'<span>'+'交易完成'+'</span>').appendTo($div);
 			var $content = $('<div/>').addClass('content').appendTo($div);
-			var $total = $('<div/>').addClass('total').html('共<span>'+goodsorder.length+'</span>件商品，合计:<span>'+total+'</span>').appendTo($div);
+			var $total = $('<div/>').addClass('total').html('共<span>'+item.num+'</span>件商品，合计:<span>'+item.num*item.price+'</span>').appendTo($div);
 			var $btn = $('<div/>').addClass('btn').html('<span>付款</span>'+'<span>删除订单</span>'+'<span>查看物流</span>').appendTo($div);
 			var $section = $('<section/>').appendTo($content);
-			    $('<img/>').attr({'src':item.imgurl}).appendTo($section);
-			    $('<div/>').html('<span>'+item.theme+'</span>'+'<span>'+item.price+'</span>').appendTo($section);
+			    $('<img/>').attr({'src':item.img}).appendTo($section);
+			    $('<div/>').html('<span>'+item.name+'</span>'+'<span>'+item.price+'</span>').appendTo($section);
 			    $('<span/>').html(item.num).appendTo($section);
 				$order_details.append($div);
 		})
